b4e047a9970a7292980ea7a6501a07614fe0d69a
Andrea Coluccio <ancolucc@microsoft.com>
2018-02-16 13:44:02 +0000

    Initial commit

---
+++ ServerApp/HelloWorldService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: ApplicationName("ServerApp")]
+[assembly: ApplicationActivation(ActivationOption.Server)]
+[assembly: ApplicationAccessControl(Value = false)]
+[assembly: System.Reflection.AssemblyKeyFile("MyTestKey.snk")]
+namespace ServerApp
+{
+    [Guid("C9D888F5-22C3-4F11-A6C4-1906E7482194")]
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("VisaHelloWorld.ServerApp2")]
+    [ComponentAccessControl(false)]
+    public class HelloWorldService : ServicedComponent
+    {
+        public string HelloWorld(string name)
+        {
+            return "Hello world from " + name;
+        }
+    }
+}
--- ServerApp/Properties/AssemblyInfo.cs
+++ ServerApp/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e4b4a1a0-b18e-424d-97ee-af1d75c1be83")]
